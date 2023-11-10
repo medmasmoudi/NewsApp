d756fffa194a4656426d1fc275ad1de0a182d786
mohamed <medmasmoudi@icloud.com>
2023-11-10 15:15:56 +0100

    initial commit

---
+++ app/src/main/java/com/example/newsappfinal/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.newsappfinal;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.newsappfinal.Models.NewsApiResponse;
+import com.example.newsappfinal.Models.NewsHeadlines;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SelectListener, View.OnClickListener{
+    RecyclerView recyclerView;
+    CustomAdapter adapter;
+    Button b1, b2, b3, b4, b5, b6, b7;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = findViewById(R.id.btn_1);
+        b1.setOnClickListener(this);
+        b2 = findViewById(R.id.btn_2);
+        b2.setOnClickListener(this);
+        b3 = findViewById(R.id.btn_3);
+        b3.setOnClickListener(this);
+        b4 = findViewById(R.id.btn_4);
+        b4.setOnClickListener(this);
+        b5 = findViewById(R.id.btn_5);
+        b5.setOnClickListener(this);
+        b6 = findViewById(R.id.btn_6);
+        b6.setOnClickListener(this);
+        b7 = findViewById(R.id.btn_7);
+        b7.setOnClickListener(this);
+
+        RequestManager manager = new RequestManager(this);
+        manager.getNewsHeadlines(listener, "general",null);
+    }
+    private final OnFetchDataListener<NewsApiResponse> listener = new OnFetchDataListener<NewsApiResponse>()  {
+        @Override
+        public void onFetchData(List<NewsHeadlines> list, String message) {
+            showNews(list);
+        }
+
+        @Override
+        public void onError(String message) {
+
+        }
+    };
+
+    private void showNews(List<NewsHeadlines> list) {
+        recyclerView = findViewById(R.id.recycler_main);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
+        adapter = new CustomAdapter(this, list, this);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void OnNewsClicked(NewsHeadlines headlines) {
+        startActivity(new Intent(MainActivity.this, DetailsActivity.class)
+                .putExtra("data",headlines));
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button button = (Button) v;
+        String category = button.getText().toString();
+
+        RequestManager manager = new RequestManager(this);
+        manager.getNewsHeadlines(listener, category,null);
+
+    }
+}
---
+++ app/src/main/java/com/example/newsappfinal/SelectListener.java
@@ -0,0 +1,7 @@
+package com.example.newsappfinal;
+
+import com.example.newsappfinal.Models.NewsHeadlines;
+
+public interface SelectListener {
+    void OnNewsClicked(NewsHeadlines headlines);
+}
